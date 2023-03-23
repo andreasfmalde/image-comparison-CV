885274c04bba671aec50490dcb55e7818d7e02d6
Sander <sandeth@stud.ntnu.no>
2023-03-23 14:02:24 +0100

    Edge detection of images, returns a value between 0 and 1 of how alike the images are in terms of edges.

--- main.m
+++ main.m
@@ -1,43 +1,60 @@
 function main()
-original_img = imread("CIDIQ\Images\Original\final01.bmp");
-blurry_img = imread("CIDIQ\Images\Reproduction\1_JPEG2000_Compression\final01_d1_l1.bmp");
+orig_image = imread("CIDIQ\Images\Original\final01.bmp");
+very_blurry_image = imread("CIDIQ\Images\Reproduction\1_JPEG2000_Compression\final01_d1_l5.bmp");
 
+edges_orig = prewitt_edge_detector_color(orig_image);
+edges_blurry = prewitt_edge_detector_color(very_blurry_image);
 
-%figure, imshowpair(original_img,blurry_img,'montage'), title("Orginal vs. Blurry");
+% Shows the images used and their edge extracted counterpart.
+figure, subplot(221), imshow(orig_image), title("Original image");
+subplot(222), imshow(very_blurry_image), title("Compressed image");
+subplot(223), imshow(edges_orig), title("Original prewitt edges");
+subplot(224), imshow(edges_blurry), title("Blurry prewitt edges");
 
-[u1 u2] = mean(original_img, blurry_img);
+% Remove 
+figure, subplot(121), imshow(abs(orig_image-very_blurry_image)), title("Original - blurry (color)");
+subplot(122), imshow(abs(edges_orig-edges_blurry)), title("Original edges - blurry edges");
 
-[covar_r,covar_g,covar_b] = covariance(original_img, blurry_img, u1, u2);
-
-end
-
-function [u1,u2] = mean(img1, img2)
-u1 = zeros(3,1);
-u1(1,:) = sum(reshape(img1(:,:,1),1,[]))/(size(img1,1)*size(img1,2));
-u1(2,:) = sum(reshape(img1(:,:,2),1,[]))/(size(img1,1)*size(img1,2));
-u1(3,:) = sum(reshape(img1(:,:,3),1,[]))/(size(img1,1)*size(img1,2));
-
-
-u2 = zeros(3,1);
-u2(1,:) = sum(reshape(img2(:,:,1),1,[]))/(size(img1,1)*size(img1,2));
-u2(2,:) = sum(reshape(img2(:,:,2),1,[]))/(size(img1,1)*size(img1,2));
-u2(3,:) = sum(reshape(img2(:,:,3),1,[]))/(size(img1,1)*size(img1,2));
+diff = sum(abs(edges_orig-edges_blurry), "all")/(size(orig_image,1)*size(orig_image,2));
+diff
 
 end
 
 
-function [covar_r,covar_g,covar_b] = covariance(img1, img2, u1, u2)
+function [output] = prewitt_edge_detector_color(image)
 
-r1 = img1(:,:,1);
-g1 = img1(:,:,2);
-b1 = img1(:,:,3);
+%convert image to double
+image = im2double(image);
 
-r2 = img2(:,:,1);
-g2 = img2(:,:,2);
-b2 = img2(:,:,3);
+%extract the three color channels
+r = image(:,:,1);
+g = image(:,:,2);
+b = image(:,:,3);
 
-covar_r = sum((r1 - u1(1)).*(r2-u2(1)))/(sum(r1) - 1);
-covar_g = sum((g1 - u1(2)).*(g2-u2(2)))/(sum(g1) - 1);
-covar_b = sum((b1 - u1(3)).*(b2-u2(3)))/(sum(b1) - 1);
+%define the kernels
+kx = [-1 0 1; -1 0 1; -1 0 1];
+ky = [-1 -1 -1; 0 0 0; 1 1 1];
+
+%apply kernels to the channels
+rx = conv2(r,kx, 'same');
+gx = conv2(g,kx, 'same');
+bx = conv2(b,kx, 'same');
+ry = conv2(r,ky, 'same');
+gy = conv2(g,ky, 'same');
+by = conv2(b,ky, 'same');
+
+%apply edge detection on the channels
+rx_edges = abs(rx) > 0.2;
+gx_edges = abs(gx) > 0.2;
+bx_edges = abs(bx) > 0.2;
+ry_edges = abs(ry) > 0.2;
+gy_edges = abs(gy) > 0.2;
+by_edges = abs(by) > 0.2;
+
+%combine the three channels
+edges = rx_edges | gx_edges | bx_edges | ry_edges | gy_edges | by_edges;
+
+%return the edges
+output = edges;
 
 end
